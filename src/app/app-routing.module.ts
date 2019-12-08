9bfc3b38b1794c2c7dc48c23e8b51ac7d5bc6c06
jesusdc99 <mbp@Jesus-MBP.local>
2019-12-08 14:23:18 +0100

    Indentación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    canActivate: [AuthGuardService], 
+    canActivate: [AuthGuardService],
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
